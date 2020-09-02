5cc2ce9436fd6e0752efcad5f1dca9210fde329d
VAIBHAV7500 <vbhvsolanki7500@gmail.com>
2020-09-02 15:28:25 +0530

    Video Update

--- backend/routes/video.js
+++ backend/routes/video.js
@@ -12,9 +12,8 @@ router.post('/',(req,res,next)=>{
     console.log(res.body);
     const file_name = 'anniversary2.mp4';
     const video = {
-        //videoUrl: `https://www3.mp4upload.com:282/d/r2xurlz7z3b4quuojsxagnkqikj3l2yedpgcsieqjiydaudqzn33xry2/rezb_7.mp4`,
-        videoUrl: `http://localhost:4200/videos/${file_name}`,
-        title: 'Anniversary Video'
+        videoUrl: `http://gdurl.com/qELA`,
+        title: 'Naruto Trailer'
     }
     res.json(video);
 });
