7cd824ad3fdf2dcb602e00245224ed34e3e6d7d0
VAIBHAV SOLANKI <vbhvsolanki7500@gmail.com>
2020-10-19 12:43:44 +0530

    Review & Player fixes

--- backend/db/db_utils.js
+++ backend/db/db_utils.js
@@ -1,5 +1,7 @@
 const runQuery = (sql,params= []) =>{
-    console.log(sql);
+    if(process.env.ENV === 'dev'){
+        console.log(sql);
+    }
     return new Promise((res,rej)=>{
         global.connection.query(sql,params,(error, result)=>{
             if(error){
