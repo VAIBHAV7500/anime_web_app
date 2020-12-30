0d6a2eb540c5fb3b8008f26aeddb0f0bd6b62e71
anubhavs <anubhavsolanki0@gmail.com>
2020-12-31 01:20:49 +0530

    updated row

--- frontend/src/components/showRoom/Row.js
+++ frontend/src/components/showRoom/Row.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from 'react'
 import "./Row.css";
 import ReactPlayer from 'react-player/lazy';
-import { MdClose, MdPlayCircleOutline } from "react-icons/md";
+import { MdClose, MdPlayCircleOutline, MdTv } from "react-icons/md";
 import {useHistory} from "react-router-dom";
-import { FaAngleDoubleDown, FaAngleLeft, FaAngleRight} from 'react-icons/fa';
+import { FaAngleDoubleDown, FaAngleLeft, FaAngleRight, FaTimes, FaYoutube} from 'react-icons/fa';
+
+let prevIndex;
 
 function Row({ title, movies, isLargeRow, rowIndex,trailerArray, showIndexArray }) {
   const [trailer, setTrailer] = trailerArray;
+  const [showIndex,setShowIndex] = showIndexArray;
+  const [showTrailer, setShowTrailer] = useState(false);
   const history = useHistory();
   let keyId = 1;
   let curTimeOut;
-  const [showIndex,setShowIndex] = showIndexArray;
   const handleClick = (movie,index) => {  
       movie.rating = Math.round(((Math.random() * (10 - 8 + 1)) + 8) * 9) / 10;
       let description = movie.description;
@@ -19,48 +22,58 @@ function Row({ title, movies, isLargeRow, rowIndex,trailerArray, showIndexArray 
           description = description.substring(0,truncLength) + '...';
       }
       movie.description = description;
-      setTrailer({...movie,index : rowIndex});
-      tiltDiv(index);   
+      console.log(movie);
+      setTrailer({...movie,index : `${rowIndex}-${index}`});
+      trailerShow(index);
   }
 
   const goToVideo = (trailer) => {
     history.push(`/show/${trailer.id}`);
   }
 
-  const handleSlider = (e) => {
-    let slide = e.target.closest('.cards');
-    slide.scrollBy(1000,0); 
-  }
-
-  const handleSliderBack = (e) => {
-    let slide = e.target.closest('.cards');
-    slide.scrollBy(-1000,0);
+  const slide = (e,direction) => {
+    let slider = document.querySelector(`[rowindex="${rowIndex}"]`);
+    if(direction === "left")
+      slider.scrollBy(-1000,0);
+    else
+      slider.scrollBy(1000,0); 
   }
 
   useEffect(() => {
-    let el = document.querySelector(`[index="${showIndex.prev}"]`);
-    if(el)
-    el.classList.remove("card_div_tilt");
-    if(showIndex.current === showIndex.prev && showIndex.current!="" && showIndex.prev!=""){
-      setShowIndex({current : "" , prev : ""});
-      setTrailer(undefined);
+    if(showIndex !== prevIndex){
+      let el = document.querySelector(`[index="${showIndex.prev}"]`);
+      if(el)
+      el.classList.remove("show_trailer");
+      if(showIndex.current === showIndex.prev && showIndex.current!="" && showIndex.prev!=""){
+        trailerRemove();
+        setTimeout(()=>{
+          setShowIndex({current : "" , prev : ""});
+          setTrailer(undefined);
+        },500);
+      }
+      prevIndex = showIndex;
     }
   }, [showIndex])
 
-  const currentTiltRemove = () => {
+  const trailerRemove = () => {
     let el = document.querySelector(`[index="${showIndex.current}"]`);
     if(el)
-    el.classList.remove("card_div_tilt");
+    el.classList.remove("show_trailer");
   }
   
-  const tiltDiv = (index) => {
+  const trailerShow = (index) => {
     let prevIndex = showIndex.current;
+    setShowTrailer(true);
     setShowIndex({
       current : `${rowIndex}-${index}`,
       prev : prevIndex
     });
     let el = document.querySelector(`[index="${rowIndex}-${index}"]`);
-    el.classList.add("card_div_tilt");
+    el.classList.add("show_trailer");
+    setTimeout(()=>{
+     const plr =  el.querySelector(".player");
+     plr.scrollIntoView({behavior: "smooth", block: "nearest", inline: "center"});
+    },300)
   }
 
   const keys = [{
@@ -77,118 +90,86 @@ function Row({ title, movies, isLargeRow, rowIndex,trailerArray, showIndexArray 
     "title": "Total Episodes"
   }];
 
-  return (
-    <div>
-      <div className="row">
-        <h2>{title}</h2>
-        < div className = "cards row_posters" >
-          <div className="slider slider_back" onClick={handleSliderBack}>
-            <FaAngleLeft/>
-          </div>
-          {movies && movies.map((movie,index) => (
-            <div key={index} index={`${rowIndex}-${index}`} name={movie.name} className={`card_div`}>
-              <div className="box-shadow"></div>
-              <MdPlayCircleOutline className="play_icon" onClick={()=>{goToVideo(movie)}}></MdPlayCircleOutline>
-              <FaAngleDoubleDown onClick={() => handleClick(movie,index)} className={`see_more`}></FaAngleDoubleDown>
-
-              <p className="show_name card_top">{movie.name}</p>
-
-              <table className="show_details card_top"> 
-              <tbody>
-                {Object.keys(movie).map(function(key) {
-                    const containskey = keys.find(x => x.key === key);
-                    if(containskey){
-                      return  (
-                      <tr>
+  const genCardOverlay = (movie,index) => {
+    return (
+      <div className={`card_overlay ${`${rowIndex}-${index}`=== trailer?.index ? "card_overlay_show" : ""} `}>
+        <div className="card_overlay_container">
+          <p className="show_name">{movie.name}</p>
+          <table className="show_details"> 
+            <tbody>
+              {Object.keys(movie).map((key,index) => {
+                  const containskey = keys.find(x => x.key === key);
+                  if(containskey){
+                    return (
+                      <tr key={index} >
                         <td>{containskey.title}</td>
                         <td className="show_detail_field">{movie[key]}</td>
                       </tr>
                     )
-                    } 
+                  } 
               })}
-                <tr>
-                  <td className="card_description" colSpan="2">{movie.description.length > 200 ? movie.description.substring(0,200) + "..." : movie.description }</td>
-                </tr>
-              </tbody>
-              </table>
-
-
-              <img
-                loading="lazy"
-                draggable="false"
-                key={keyId++}
-                onClick={()=>{goToVideo(movie)}}
-                className={`row_poster  card ${isLargeRow && "row_posterLarge"}`}
-                src = {
-                  `${isLargeRow ? movie.poster_portrait_url.replace('medium','large') : movie.poster_landscape_url}`
-                }
-                alt={movie.name}
-              />
-            </div>
-          ))}
-          <div className="slider slider_front" onClick={handleSlider}>
-            <FaAngleRight/>
+              <tr>
+                <td className="card_description" colSpan="2">{movie.description.length > 150 ? movie.description.substring(0,150) + "..." : movie.description }</td>
+              </tr>
+            </tbody>
+          </table>
+          <div onClick={() => { handleClick(movie,index)  }} className="card_btn top_overlay_btn">
+            {`${rowIndex}-${index}`=== trailer?.index ? <FaTimes/> : <MdTv/>}
+            <span>{`${rowIndex}-${index}`=== trailer?.index ? "Close" : "Play"} Trailer</span>
+          </div>
+          <div onClick={()=>{goToVideo(movie)}} className="card_btn watch_now_btn">
+            <MdPlayCircleOutline className="watch_now_icon"/>
+            <span>WATCH NOW!</span>
           </div>
         </div>
       </div>
-      {
-          window.screen.availWidth > 514 ?( trailer && rowIndex==trailer.index && <div className="trailer_window" id="trailer_window">
-          < MdClose className = "close-btn" onClick={()=>{setTrailer(undefined);currentTiltRemove();setShowIndex({prev : "",current : ""})}}/>
-          < ReactPlayer  
-            url={`${trailer.trailer_url}`} 
-            height = "390px"
-            width = "640px"
-            playing= {true}
-            className="player"
-          />
-          <div className="info"> 
-            <div className="trailer_name">{trailer.name}</div><br/>
-            <table className="info_table">
-                <tbody>
-                  <tr className="trailer_row">
-                    <td>
-                        Season: 
-                    </td>
-                    <td className="season">
-                        {trailer.season}
-                    </td>
-                  </tr>
-                  < tr className = "trailer_row" >
-                    <td>
-                      Rating: 
-                    </td>
-                    < td  >
-                      <div className = {
-                      `rating rating_${Math.ceil(trailer.rating/3)}`
-                    }>{trailer.rating}/10</div>
-                    </td>
-                  </tr>
-                  <tr className="trailer_row">
-                    <td>
-                        Genre: 
-                    </td>
-                    <td className="genre">
-                        {trailer.genres?.join(', ') || 'Comedy, Adventure, Mystery, Thriller'}
-                    </td>
-                  </tr>
-                  <tr className="trailer_row">
-                    <td>
-                        Description: 
-                    </td>
-                    { trailer.description !== '' && <td className="description">
-                        {
-                          trailer.description}
-                    </td> }
-                  </tr>
-                </tbody>
-            </table>
-            < button className = "trailer_btn neumorphism"  onClick={()=>{goToVideo(trailer)}}>Go to Show
-            </button>
-            < button className = "trailer_btn" > Add to WatchList
-            </button>
-          </div>
-        </div>) : trailer && goToVideo(trailer) 
-      }
+    )
+  }
+
+  return (
+    <div className="row_container">
+      <h2 className="row_title">{title}</h2>
+      <div className={`row_wrapper`}>
+      <div className="slider slider_back" onClick={(e)=>{slide(e,"left")}}>
+        <FaAngleLeft/>
+      </div>
+      <div className="slider slider_front" onClick={(e)=>{slide(e,"right")}}>
+      <FaAngleRight/>
+      </div>
+      <div rowindex={rowIndex} className="row">
+        <div className = "cards row_posters" >
+          {movies && movies.map((movie,index) => (
+            <div key={index} index={`${rowIndex}-${index}`} className={`card_wrapper`}>
+              <div name={movie.name} className={`card_div`}>
+                {genCardOverlay(movie,index)}
+                <img
+                  loading="lazy"
+                  draggable="false"
+                  key={keyId++}
+                  onClick={()=>{goToVideo(movie)}}
+                  className={`row_poster  card ${isLargeRow && "row_posterLarge"}`}
+                  src = {
+                    `${isLargeRow ? movie.poster_portrait_url.replace('medium','large') : movie.poster_landscape_url}`
+                  }
+                  alt={movie.name}
+                />
+              </div>
+              {
+                `${rowIndex}-${index}`=== trailer?.index &&
+                <ReactPlayer  
+                  url={`${trailer?.trailer_url}`} 
+                  width="500px"
+                  height="100%"
+                  playing= {true}
+                  className="player"
+                />
+              }
+            </div>
+          ))}
+          
+        </div>
+      </div>
+    </div>
     </div>
   );
 }
