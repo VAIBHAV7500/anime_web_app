cdd9d4e4b8552d3ea362a4aa59659d58718991ee
VAIBHAV7500 <vbhvsolanki7500@gmail.com>
2021-12-01 15:59:24 +0530

    Build

---
+++ frontend/build/precache-manifest.bfa6a36d2a38f6ede28cc17ebed64a4f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c259bf24b8ef9dc852d9d6f5f18e6623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd1277f03c056a8978aa",
+    "url": "/static/css/2.057b75c7.chunk.css"
+  },
+  {
+    "revision": "64f008fa1d3a5b6eabea",
+    "url": "/static/css/main.f4912a9a.chunk.css"
+  },
+  {
+    "revision": "bd1277f03c056a8978aa",
+    "url": "/static/js/2.9c349375.chunk.js"
+  },
+  {
+    "revision": "b734a382e63ed01396a92a7758b8f0e5",
+    "url": "/static/js/2.9c349375.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64f008fa1d3a5b6eabea",
+    "url": "/static/js/main.db40599b.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  }
+]);
