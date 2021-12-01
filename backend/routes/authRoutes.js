181e2f4562359118181f7c0ccf1122f107ec5ce2
VAIBHAV7500 <vbhvsolanki7500@gmail.com>
2021-12-01 13:26:08 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -8,7 +8,7 @@ const {logger} = require('../lib/logger');
 
 module.exports = (app)=>{
     
-    router.post('/register',geoBlockCheckMiddleware, userSchemaCheck , async (req, res, next) => {
+    router.post('/register', userSchemaCheck , async (req, res, next) => {
         try{
             let body = req.body;
             const password = body.password;
