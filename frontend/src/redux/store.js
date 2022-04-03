59c4e63ddf8fafaa388c757ed61b4d2efbb4f984
VAIBHAV SOLANKI <vbhvsolanki7500@gmail.com>
2022-04-03 12:18:04 +0530

    Update store.js

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,7 +2,7 @@ import {createStore, applyMiddleware} from 'redux';
 import authReducer from './Auth/authReducer';
 import logger from 'redux-logger';
 
-const store = createStore(authReducer,applyMiddleware(logger));
+const store = createStore(authReducer);
 
 export default store;
 
